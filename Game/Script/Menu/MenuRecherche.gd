831ee5ebeb5f2369345d4e826bf3c22fdf567650
Tom <44830205+loquils@users.noreply.github.com>
2024-08-04 12:35:25 +0200

    Recherches UI + Fix
    Ajout des recherche "Debut", fix de l'UI en conséquence

--- Game/Script/Menu/MenuRecherche.gd
+++ Game/Script/Menu/MenuRecherche.gd
@@ -3,20 +3,32 @@ extends Control
 var ButtonRechercheScenePreload = preload("res://Design/Scenes/ButtonRecherche.tscn")
 
 #@onready var ResearchesEasyContainer = $MainMarginC/ResearchLvlVBoxC/ScrollRecherchesC/ResearchesEasyHBoxC
-@onready var ResearchesEasyContainer = $MainMarginC/ResearchLvlVBoxC/ScrollContainer/GridContainer
+@onready var ResearchesDebutContainer = $MainMarginC/ResearchLvlVBoxC/LvlDebutScrollC/GridC
+@onready var ResearchesEasyContainer = $MainMarginC/ResearchLvlVBoxC/LvlEasyScrollC/GridC
+
+@onready var RechercheVBoxC = $MainMarginC/ResearchLvlVBoxC
+
+var ListeScrollCNiveaux
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#On connecte ici l'appuie du bouton lors de l'achat d'une recherche
 	RechercheClick.connect("Research_button_pressed", AchatRehercheButtonPressed)
 	
+	#On récupère tous les ScrollC de la vue pour afficher/cacher
+	ListeScrollCNiveaux = RechercheVBoxC.get_children()
+	ListeScrollCNiveaux.remove_at(0)
+	
 	for recherche in RessourceManager.ListeRecherches:
 		var newRecherchebutton = ButtonRechercheScenePreload.instantiate()
 		newRecherchebutton._set_var(recherche)
 		match recherche.ResearchLevel:
+			Recherche.ResearchLevelEnum.DEBUT:
+				ResearchesDebutContainer.add_child(newRecherchebutton)
 			Recherche.ResearchLevelEnum.EASY:
 				ResearchesEasyContainer.add_child(newRecherchebutton)
-		
+	
 	BonusManager.MajBonusRecherches()
 
 
@@ -37,8 +49,11 @@ func AchatRehercheButtonPressed(recherche):
 
 	BonusManager.MajBonusRecherches()
 
-
-var isEasyButtonExpanded = true
-func _on_expand_lvl_easy_button_pressed():
-	isEasyButtonExpanded = not isEasyButtonExpanded
-	ResearchesEasyContainer.visible = isEasyButtonExpanded
+#Methode apellee lors de l'appuie sur un des boutons de niveau de recherche
+#Permet d'afficher les bonnes recherches, et de cacher les autres
+func _on_expand_lvl_button_pressed(extra_arg_0):
+	for scrollC in ListeScrollCNiveaux:
+		if scrollC.name.contains(extra_arg_0):
+			scrollC.show()
+		else:
+			scrollC.hide()
