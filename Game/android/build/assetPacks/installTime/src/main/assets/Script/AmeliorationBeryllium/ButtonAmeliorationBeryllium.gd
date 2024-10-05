70115c7478f2f14492bf2c975f860d60a91bb718
Tom <44830205+loquils@users.noreply.github.com>
2024-10-06 00:10:57 +0200

    Build 0.0.23

--- Game/android/build/assetPacks/installTime/src/main/assets/Script/AmeliorationBeryllium/ButtonAmeliorationBeryllium.gd
+++ Game/android/build/assetPacks/installTime/src/main/assets/Script/AmeliorationBeryllium/ButtonAmeliorationBeryllium.gd
@@ -17,6 +17,17 @@ var AmeliorationBeryllium: AmeliorationBeryllium
 @onready var UnlockAtomePrixLabel = $PanelForUnlock/FondPanel/VBoxContainer/PrixLabel
 @onready var AmeliorationBerylliumUnlockButton= $PanelForUnlock/FondPanel/VBoxContainer/AmeliorationBerylliumUnlockButton
 
+@onready var AllAmeliorationsVBoxC = $MainMarginC/PresVBoxC/DetailsMarginC/VBoxC
+@onready var BerylliumAmeliorationVBoxC = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC
+@onready var BerylliumCurrentHydrogenBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC/HydrogeneFondPanel/MarginC/VBoxC/VBoxC/BonusActuelLabel
+@onready var BerylliumNextHydrogenBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC/HydrogeneFondPanel/MarginC/VBoxC/VBoxC/BonusSuivantLabel
+@onready var BerylliumCurrentHeliumBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC/HeliumFondPanel/MarginC/VBoxC/VBoxC/BonusActuelLabel
+@onready var BerylliumNextHeliumBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC/HeliumFondPanel/MarginC/VBoxC/VBoxC/BonusSuivantLabel
+@onready var BerylliumCurrentLithiumBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC2/LithiumFondPanel/MarginC/VBoxC/VBoxC/BonusActuelLabel
+@onready var BerylliumNextLithiumBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC2/LithiumFondPanel/MarginC/VBoxC/VBoxC/BonusSuivantLabel
+@onready var BerylliumCurrentBerylliumBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC2/BerylliumFondPanel/MarginC/VBoxC/VBoxC/BonusActuelLabel
+@onready var BerylliumNextBerylliumBonusLabel = $MainMarginC/PresVBoxC/DetailsMarginC/BerylliumVBoxC/HBoxC2/BerylliumFondPanel/MarginC/VBoxC/VBoxC/BonusSuivantLabel
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -48,12 +59,33 @@ func _process(_delta):
 		
 		for i in range(0,4):
 			finalLevels[i] = Big.add(bonusesAmelioration[i], bonusesAdd[i])
-		
-		BonusHydrogeneLabel.text = str(finalLevels[0])
-		BonusHeliumLabel.text = str(finalLevels[1])
-		BonusLithiumLabel.text = str(finalLevels[2])
-		BonusBerylliumLabel.text = str(finalLevels[3])
-		
+			
+		if AmeliorationBeryllium.Id != 3:
+			if BerylliumAmeliorationVBoxC.visible:
+				BerylliumAmeliorationVBoxC.hide()
+			if not AllAmeliorationsVBoxC.visible:
+				AllAmeliorationsVBoxC.show()
+			
+			BonusHydrogeneLabel.text = str(finalLevels[0])
+			BonusHeliumLabel.text = str(finalLevels[1])
+			BonusLithiumLabel.text = str(finalLevels[2])
+			BonusBerylliumLabel.text = str(finalLevels[3])
+
+		else:
+			if not BerylliumAmeliorationVBoxC.visible:
+				BerylliumAmeliorationVBoxC.show()
+			if AllAmeliorationsVBoxC.visible:
+				AllAmeliorationsVBoxC.hide()
+			
+			BerylliumCurrentHydrogenBonusLabel.text = "+" + str(bonusesAdd[0])
+			BerylliumNextHydrogenBonusLabel.text = "+" + str(Big.add(Big.new(bonusesAdd[0]), bonusesAmelioration[0]))
+			BerylliumCurrentHeliumBonusLabel.text = "+" + str(bonusesAdd[1])
+			BerylliumNextHeliumBonusLabel.text = "+" + str(Big.add(Big.new(bonusesAdd[1]), bonusesAmelioration[1]))
+			BerylliumCurrentLithiumBonusLabel.text = "+" + str(bonusesAdd[2])
+			BerylliumNextLithiumBonusLabel.text = "+" + str(Big.add(Big.new(bonusesAdd[2]), bonusesAmelioration[2]))
+			BerylliumCurrentBerylliumBonusLabel.text = "+" + str(bonusesAdd[3])
+			BerylliumNextBerylliumBonusLabel.text = "+" + str(Big.add(Big.new(bonusesAdd[3]), bonusesAmelioration[3]))
+			
 		NiveauLabel.text = tr("Niv.") + str(AmeliorationBeryllium.Level)
 		var prix = AmeliorationBeryllium.GetPrixAmeliorationBeryllium()
 		PrixLabel.text = tr(prix.keys()[0])  + ": " + str(prix.values()[0])
