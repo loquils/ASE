902fc66f6f34956a065127a047af4a19ce8f8de3
Tom <44830205+loquils@users.noreply.github.com>
2024-12-07 11:21:42 +0100

    Correction
    Oublie Unlock sur amélio bore

--- Game/Script/AmeliorationBore/ButtonAdvancedAmeliorationBore.gd
+++ Game/Script/AmeliorationBore/ButtonAdvancedAmeliorationBore.gd
@@ -10,10 +10,7 @@ var AmeliorationBore
 @onready var BonusActuelLabel = $PresentationPanel/PresentationVBoxC/ContenuHBoxC/RightMarginC/PanelC/VBoxC/BonusActuelMarginC/BonusActuel
 @onready var ButtonAmeliorationBore = $Button
 
-@onready var UnlockPanel = $PanelForUnlock
-@onready var UnlockAtomeNomLabel = $PanelForUnlock/FondPanel/VBoxContainer/AtomeLabel
-@onready var UnlockAtomePrixLabel = $PanelForUnlock/FondPanel/VBoxContainer/PrixLabel
-@onready var AmeliorationBoreUnlockButton= $PanelForUnlock/FondPanel/VBoxContainer/AmeliorationBoreUnlockButton
+@onready var UnlockPanel = $UnlockControl
 
 #Définition de l'UI du bouton personnalisé.
 func _set_var(ameliorationBore):
@@ -22,8 +19,9 @@ func _set_var(ameliorationBore):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	UnlockPanel._set_objects(AmeliorationBore)
 	ButtonAmeliorationBore.pressed.connect(RechercheClick.AmeliorationBoreButtonEventTrigger.bind(AmeliorationBore))
-	UnlockAtomePrixLabel.text = str(AmeliorationBore.AtomePriceForUnlocking.values()[0])
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -37,19 +35,11 @@ func _process(delta):
 	BonusAmeliorationLabel.text = "Bonus/Niv +" + str(Big.multiply(BonusManager.GetAmeliorationBoreQuantiteeUniqueBonus(AmeliorationBore), Big.new(1.0, 2))) + "%"
 	
 	if UnlockPanel.visible:
-		UnlockAtomeNomLabel.text = tr(AmeliorationBore.AtomePriceForUnlocking.keys()[0])
-		
 		if AmeliorationBore.IsUnlocked:
 			UnlockPanel.visible = false
-		
-		if RessourceManager.QuantiteesAtomes[AmeliorationBore.AtomePriceForUnlocking.keys()[0]].isLessThan(AmeliorationBore.AtomePriceForUnlocking.values()[0]):
-			AmeliorationBoreUnlockButton.disabled = true
-		else:
-			AmeliorationBoreUnlockButton.disabled = false
 	else:
 		if not AmeliorationBore.IsUnlocked:
 			UnlockPanel.visible = true
-			return
 	
 	if AmeliorationBore.IsUnlocked:
 		if RessourceManager.QuantiteesAtomes.has("Bore"):
@@ -57,19 +47,3 @@ func _process(delta):
 				ButtonAmeliorationBore.disabled = true
 			else : 
 				ButtonAmeliorationBore.disabled = false
-
-
-#Lors du clic pour unlock une recherche de bore, enlève le bore (prix) et débloque la recherche.
-func _on_amelioration_bore_unlock_button_pressed():
-	print("Bouton Unlock bore amélioration :" + AmeliorationBore.Name)
-	if AmeliorationBore.IsUnlocked:
-		return
-	
-	if RessourceManager.QuantiteesAtomes[AmeliorationBore.AtomePriceForUnlocking.keys()[0]].isLessThan(AmeliorationBore.AtomePriceForUnlocking.values()[0]):
-		return
-			
-	RessourceManager.QuantiteesAtomes[AmeliorationBore.AtomePriceForUnlocking.keys()[0]] = Big.subtractAbove0(RessourceManager.QuantiteesAtomes[AmeliorationBore.AtomePriceForUnlocking.keys()[0]], AmeliorationBore.AtomePriceForUnlocking.values()[0])
-		
-	AmeliorationBore.IsUnlocked = true
-	
-	BonusManager.MajBonusAmeliorationBore()
